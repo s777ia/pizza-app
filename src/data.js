56a8fc6706aff394e8907c46fd5904f5154aee1a
s777ia <23046128@myrp.edu.sg>
2024-11-11 15:39:07 +0800

    FIRST COMMIT

---
+++ src/data.js
@@ -0,0 +1,10 @@
+const pizzas = [
+    { image: 'pizzas/spinaci.jpg', name: 'Spinach Pizza', ingredients: 'Tomato, Mozzarella, Spinach, and Ricotta Cheese', price: 10},
+    { image:'pizzas/funghi.jpg', name: 'Funghi Pizza', ingredients: 'Tomato, Mozzarella, Mushrooms, and Onions', price: 12 },
+    { image: 'pizzas/margherita.jpg', name: 'Magherita Pizza', ingredients: 'Tomato, Mozzarella, Basil', price: 9 },
+    { image: 'pizzas/focaccia.jpg', name: 'Focaccia', ingredients: 'Bread with Italian Olive Oil and Rosemary', price: 6 },
+    { image: 'pizzas/salamino.jpg', name: 'Salamino Pizza', ingredients: 'Tomato, Mozarella, and Pepperoni', price: 15 },
+    { image: 'pizzas/prosciutto.jpg', name: 'Proscuitto Pizza', ingredients: 'Tomato, Mozarella, Ham, Argula, and Burrata Cheese', price: 18 }
+]
+
+export default pizzas;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./style.css";
+import "./index.css"
+import pizzas from "./data.js"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+    return (
+        <div className='container'>
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Header() {
+    return <h1>Sonia's Pizza Co.</h1>;
+}
+
+function Pizza(props) {
+    return (
+        <div className="pizza">
+            <img src={props.image} alt={props.name} />
+            <h2>{props.name}</h2>
+            <p>{props.ingredients}</p>
+            <p>{props.price}</p>
+        </div>
+    );
+}
+
+function Menu() {
+    return (
+        <div className='menu'>
+            <h1>Our Menu</h1>
+            <div className='card'>
+                {pizzas.map((pizza) => (
+                    <Pizza
+                        image={pizza.image}
+                        name={pizza.name}
+                        ingredients={pizza.ingredients}
+                        price={pizza.price}
+                    />
+                ))}
+                </div>
+        </div>
+    );
+}
+
+
+function Order() {
+    return (
+        <>
+        <p>We're currently open!</p>
+        <button class="btn">Order</button>
+        </>
+    )
+}
+
+function Footer() {
+    const date = new Date();
+    const hours = date.getHours();
+    const isOpen = hours >= 10 && hours < 22;
+    
+    return (
+        <footer className='footer'>
+            {isOpen ? <Order /> : "Sorry we're closed"}
+        </footer>
+    );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
